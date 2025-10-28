8988ef399d1aff6268ef4623e07ed27db2c41bf0
Liran Schour <lirans@il.ibm.com>
2025-10-28 09:46:37 +0200

    n/a

--- vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/nixl_connector.py
@@ -1500,13 +1500,14 @@ class NixlConnectorWorker:
         block_id = 0
         layer = 0
         blocks = len(local_block_ids)
-        for i, block in enumerate(local_block_descs_ids):
-            print(f"XXX Block: Layer-{layer}:{block_id}-->{block} To remote block {remote_block_descs_ids[i]} len since prev addr: {self.src_xfer_side_blocks[block][0] - prev_addr} len: {self.src_xfer_side_blocks[block][1]}")
-            prev_addr = self.src_xfer_side_blocks[block][0]
-            block_id += 1
-            if block_id % blocks == 0:
-                block_id = 0
-                layer += 1
+        if False:
+            for i, block in enumerate(local_block_descs_ids):
+                print(f"XXX Block: Layer-{layer}:{block_id}-->{block} To remote block {remote_block_descs_ids[i]} len since prev addr: {self.src_xfer_side_blocks[block][0] - prev_addr} len: {self.src_xfer_side_blocks[block][1]}")
+                prev_addr = self.src_xfer_side_blocks[block][0]
+                block_id += 1
+                if block_id % blocks == 0:
+                    block_id = 0
+                    layer += 1
 
         # Prepare transfer with Nixl.
         handle = self.nixl_wrapper.make_prepped_xfer(
